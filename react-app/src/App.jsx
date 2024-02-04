de7346ec98f218e895c1929a4a189d7e214fe5e3
Saba Tahmasebi <saba.tms15@gmail.com>
2024-02-03 21:48:52 -0800

    src

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,9 +1,12 @@
+import Test from "./Test.jsx"
+
 function App() {
   return (
     <div>
      <h1>Booto start</h1>
      <p>React Course</p>
      <p>{ 4 * 5}</p>
+     <Test />
     </div>
    )
 }
